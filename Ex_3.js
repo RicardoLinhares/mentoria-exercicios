cd034a58cd082d1d3868497369cda152ea96e4bb
RicardoLinhares <eng.ricardohdl@gmail.com>
2021-01-28 13:44:32 -0300

    Ex_3.js

---
+++ Ex_3.js
@@ -0,0 +1,14 @@
+/* Calcular número ao quadrado */
+function calculaIdade(n) {
+    
+    if(n < 18) {
+        console.log("Você não tem idade para beber")
+    }
+
+    else {
+        console.log("Você pode beber")
+    }
+    return
+}
+
+calculaIdade(60)
